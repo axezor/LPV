daa9066daeeadd60e6f7d8f0109671e741452f61
axezor <malcaninja@gmail.com>
2015-04-26 21:51:09 +0700

    Update LocalPictureViewer.meta.js
    skip if can't gen div viewer

--- script/LocalPictureViewer.meta.js
+++ script/LocalPictureViewer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Local Picture Viewer
 // @namespace    http://axezor.blogspot.com/p/lpv.html
-// @version      3.1.4
+// @version      3.1.5
 // @description  enjoy. :P
 // @author       AxEzOr
 // @match        file:///*
