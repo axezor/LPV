9f26d495eeec544e6b9932aad4b6a6905a81aafe
axezor <malcaninja@gmail.com>
2015-06-17 03:59:42 +0700

    Update LocalPictureViewer.meta.js

--- script/LocalPictureViewer.meta.js
+++ script/LocalPictureViewer.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Local Picture Viewer
 // @namespace    http://axezor.blogspot.com/p/lpv.html
-// @version      3.1.8
+// @version      3.1.9
 // @description  enjoy. :P
 // @author       AxEzOr
 // @match        file:///*
